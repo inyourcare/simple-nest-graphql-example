82febdcf6801e03a4a07b09b224dd98017c7744d
kwonkihoon <inyourcareallen@gmail.com>
2020-09-08 16:04:23 +0900

    typeorm added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'test',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
 })
 export class AppModule {}
